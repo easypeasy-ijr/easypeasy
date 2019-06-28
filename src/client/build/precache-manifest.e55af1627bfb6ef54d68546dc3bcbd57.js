c376589bfad7834af16487e70025a7f90620535d
rbsrafa <rbsrafa@gmail.com>
2019-06-28 12:04:17 +0100

    added react build folder

---
+++ src/client/build/precache-manifest.e55af1627bfb6ef54d68546dc3bcbd57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8ba2f0746fe38ee55fc9b01b2e235c2",
+    "url": "/static/media/cart.f8ba2f07.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b45ecb88c5332a503581",
+    "url": "/static/js/main.b45ecb88.chunk.js"
+  },
+  {
+    "revision": "8698aeeeda770971f9c2",
+    "url": "/static/js/1.8698aeee.chunk.js"
+  },
+  {
+    "revision": "b45ecb88c5332a503581",
+    "url": "/static/css/main.9a0a299d.chunk.css"
+  },
+  {
+    "revision": "eadbd0f50e4a554aff192f156fc9b0b7",
+    "url": "/index.html"
+  }
+];
